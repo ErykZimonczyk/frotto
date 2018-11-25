21aefd4095dbfb1b9a273941ca99b461c76054f7
GRUPAONET\kwojtanowski <kamil.wojtanowski@dreamlab.pl>
2018-11-25 05:22:25 +0100

    clustering user points

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -251,6 +251,37 @@ export class MapComponent implements OnInit, OnDestroy {
 		});
 
 		this.map.addLayer({
+			id: 'user-clusters',
+			type: 'circle',
+			source: 'userBets',
+			filter: ['has', 'point_count'],
+			paint: {
+				'circle-color': '#27ae60',
+				'circle-radius': [
+					'step',
+					['get', 'point_count'],
+					20,
+					100,
+					30,
+					750,
+					40,
+				],
+			},
+		});
+
+		this.map.addLayer({
+			id: 'user-cluster-count',
+			type: 'symbol',
+			source: 'userBets',
+			filter: ['has', 'point_count'],
+			layout: {
+				'text-field': '{point_count_abbreviated}',
+				'text-font': ['DIN Offc Pro Medium', 'Arial Unicode MS Bold'],
+				'text-size': 12,
+			},
+		});
+
+		this.map.addLayer({
 			id: 'unclustered-point',
 			type: 'circle',
 			source: 'bets',
